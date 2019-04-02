3370baa40b6862d8b6dd91310d9903012ff6d28e
Malcolm Gourdine <malcolmgourdine@gmail.com>
2019-04-01 21:02:17 -0400

    with Router added

--- pages/post.js
+++ pages/post.js
@@ -1,7 +1,10 @@
 import Layout from '../component/Layout';
+import withRouter  from 'next/router';
 
-export default ({ url }) => (
-  <Layout title ={url.query.title}>
+const Post = ({router }) => (
+  <Layout title ={router.query.title}>
     <p>Blaoroeib ergbuew glwejrgeoj gej glewj rgwegew   ergwe</p>
   </Layout>
 )
+ 
+export default withRouter(Post);
